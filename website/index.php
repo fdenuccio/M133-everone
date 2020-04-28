e711f6162246558e8ddad62e53111eec91b67f91
siculu33 <62589975+siculu33@users.noreply.github.com>
2020-04-28 09:40:48 +0200

    Update index.php

--- website/index.php
+++ website/index.php
@@ -1,9 +1,9 @@
 <?php
 <a href="./php/register.php">register</a>
 <a href="./php/login.php">login</a>
-
+<center>
   // Logo
-<img src="bild.jpg" />
-  
+<img src="./images/everone-logo.png"/>
+</center>
   
 ?>
