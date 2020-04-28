731b4d85e2a3ef6167ccfa43ac2a0bab04b6f3ac
New-e <54321198+New-e@users.noreply.github.com>
2020-04-28 09:09:27 +0200

    Update impressum.php

--- impressum.php
+++ impressum.php
@@ -1,1 +1,10 @@
+<h4><a name="Musterimpressum_Einzelfirma"><span style="font-size: x-small;"><span style="font-family: Verdana;"> </span><span style="font-size: small;"><span style="font-family: Verdana;">Musterimpressum für Einzelunternehmer</span></span></span></a></h4>
 
+<span style="font-family: Verdana;">
+Vorname und Name Max Mustermann<br>
+Strasse und Hausnummer Musterstrasse 1<br>
+Postleitzahl und Ort 8000 Musterstadt Schweiz<br>
+Telefonnummer 044 000 00 00<br>
+ggf. Fax wenn vorhanden oder gewünscht<br>
+E-Mail Max.Muster@……. ch<br>
+Umsatzsteuer-Identifikationsnummer wenn vorhanden </span>
